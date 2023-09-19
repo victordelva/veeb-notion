a018fee54e825aae9f0ac3137046b065f608baee
Victor <vicalle49@gmail.com>
2023-09-19 11:48:43 +0200

    home wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import 'styles/global.css'
+import 'styles/custom.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,17 @@
 import * as React from 'react'
 
-import { NotionPage } from '@/components/NotionPage'
-import { domain } from '@/lib/config'
-import { resolveNotionPage } from '@/lib/resolve-notion-page'
+import {PageHead} from "@/components/PageHead";
 
-export const getStaticProps = async () => {
-  try {
-    const props = await resolveNotionPage(domain)
-
-    return { props, revalidate: 10 }
-  } catch (err) {
-    console.error('page error', domain, err)
-
-    // we don't want to publish the error version of this page, so
-    // let next.js know explicitly that incremental SSG failed
-    throw err
-  }
-}
-
-export default function NotionDomainPage(props) {
-  return <NotionPage {...props} />
+export default function Home(props) {
+  return <div className='home-wrapper'>
+    <PageHead
+      title={'blexity'}
+      description={'Softare development and consulting'}
+    />
+    <div className="home-container">
+      <h1>blexity</h1>
+      <p>Software development and consulting</p>
+      <a href="mailto:info@blexity.com">info@blexity.com</a>
+    </div>
+  </div>
 }
